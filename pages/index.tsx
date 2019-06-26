77b8e91fc3b93b63d7cf614feddc15c187be24fe
Eliot <saltycrane@gmail.com>
2019-06-26 15:45:08 -0700

    example showing immer reducer loses type with useReducer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,27 @@
-import React from "react";
+import produce, { Draft } from "immer";
+import React, { useReducer } from "react";
 
-const Home = () => <div>Home</div>;
+type State = number;
+
+const reducer1 = (state: State) => state;
+
+const reducer2 = produce((draft: Draft<State>) => {
+  draft + 1;
+  return;
+});
+
+const Home = () => {
+  // state1 is correctly typed as `number`
+  const [state1] = useReducer(reducer1, 0);
+
+  // state2 is incorrectly typed as `any`
+  const [state2] = useReducer(reducer2, 0);
+
+  return (
+    <div>
+      {state1} {state2}
+    </div>
+  );
+};
 
 export default Home;
