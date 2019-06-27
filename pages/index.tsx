4bdcb4cfa9b67b9719bc2c5740deef53e45d8cea
Eliot <saltycrane@gmail.com>
2019-06-26 17:17:43 -0700

    the `use-immer` package preserves the type of my state

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import produce, { Draft } from "immer";
 import React, { useReducer } from "react";
+import { useImmerReducer } from "use-immer";
 
 type State = number;
 
@@ -10,6 +11,8 @@ const reducer2 = produce((draft: Draft<State>) => {
   return;
 });
 
+const reducer3 = (state: State) => state;
+
 const Home = () => {
   // state1 is correctly typed as `number`
   const [state1] = useReducer(reducer1, 0);
@@ -17,9 +20,12 @@ const Home = () => {
   // state2 is incorrectly typed as `any`
   const [state2] = useReducer(reducer2, 0);
 
+  // state3 is correctly typed as `number`
+  const [state3] = useImmerReducer(reducer3, 0);
+
   return (
     <div>
-      {state1} {state2}
+      {state1} {state2} {state3}
     </div>
   );
 };
